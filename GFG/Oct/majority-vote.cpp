33ed71aa647123206bfeb3bfee0488c79ff7beff
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2024-10-03 14:38:57 +0530

    Create majority-vote.cpp

---
+++ GFG/Oct/majority-vote.cpp
@@ -0,0 +1,19 @@
+ vector<int> findMajority(vector<int>& nums) {
+    unordered_map<int,int> map;
+    for(auto i: nums){
+        map[i]++;
+    }
+    int limit = nums.size()/3;
+    
+    vector<int> output;
+    int flag=0;
+    for(auto i:map){
+        if(i.second>limit){
+            output.push_back(i.first);
+            flag=1;
+        }
+    }
+    if(flag==0)output.push_back(-1);
+    sort(output.begin(),output.end());
+    return output;
+}

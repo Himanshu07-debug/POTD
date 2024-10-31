eb22161651e7a9bbcfe0aa7641084c8ca0ecafb0
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2024-10-31 22:20:08 +0530

    Create insert-in-a-sorted-dll.cpp

---
+++ GFG/Oct/insert-in-a-sorted-dll.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    Node* sortedInsert(Node* head, int x) {
+        // Code here
+        Node *n = new Node;
+        n->data = x;
+        n->prev = NULL;
+        n->next = NULL;
+        
+        Node*pre=NULL;
+        Node*curr=head;
+        if(curr->data>=x)
+        {
+            
+            n->next=curr;
+            return n;
+        }
+        while(curr!=NULL)
+        {
+            if(curr->data<=x)
+            {
+                pre=curr;
+                curr=curr->next;
+            }
+            else{
+                break;
+            }
+        }
+        pre->next=n;
+        pre=pre->next;
+        pre->next=curr;
+        return head;
+        
+    }
+};

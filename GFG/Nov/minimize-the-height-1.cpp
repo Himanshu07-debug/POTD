35093da6662c34e3f501f0056423edab7a006d61
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2024-11-23 22:39:47 +0530

    Create minimize-the-height-1.cpp

---
+++ GFG/Nov/minimize-the-height-1.cpp
@@ -0,0 +1,18 @@
+int getMinDiff(int k, vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    
+    int n = arr.size();
+    int ans = arr[n-1] - arr[0];
+    
+    int mi, ma; 
+    
+    for (int i = 1; i < n; i++) {
+        ma = max(arr[n-1] - k, arr[i-1] + k);
+        
+        mi = min(arr[0] + k, arr[i] - k);
+        
+        ans = min(ans, ma - mi);
+    }
+    
+    return ans;
+}

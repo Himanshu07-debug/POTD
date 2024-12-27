d8962d24443a346bc3b958ce09c69ed0c20a3e55
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2024-12-27 10:06:12 +0530

    Create cnt-pair-with-given-sum.cpp

---
+++ GFG/Dec/cnt-pair-with-given-sum.cpp
@@ -0,0 +1,15 @@
+int countPairs(vector<int> &arr, int target)
+{
+    // Code here
+    int count = 0;
+    unordered_map<int, int> mp;
+    for (int i : arr)
+    {
+        if (mp.find(target - i) != mp.end())
+        {
+            count += mp[target - i];
+        }
+        mp[i]++;
+    }
+    return count;
+}

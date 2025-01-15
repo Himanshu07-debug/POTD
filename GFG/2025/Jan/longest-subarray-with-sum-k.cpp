f14f319fd42ebe7d292b87c3f085580015d18331
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2025-01-15 08:53:19 +0530

    Create longest-subarray-with-sum-k.cpp

---
+++ GFG/2025/Jan/longest-subarray-with-sum-k.cpp
@@ -0,0 +1,26 @@
+int longestSubarray(vector<int> &arr, int k)
+{
+    // code here
+    int sum = 0;
+    int maxlen = 0;
+    unordered_map<int, int> map;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+        if (sum == k)
+        {
+            maxlen = i + 1;
+        }
+        if (map.find(sum - k) != map.end())
+        {
+            maxlen = max(maxlen, i - map[sum - k]);
+        }
+        if (map.find(sum) == map.end())
+        {
+            map[sum] = i;
+        }
+    }
+
+    return maxlen;
+}

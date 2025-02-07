ad473af37f765e1f33c1f62fb2a9baa8a4dcb2a9
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2025-02-07 21:37:40 +0530

    Create inorder.cpp

---
+++ GFG/2025/Feb/inorder.cpp
@@ -0,0 +1,19 @@
+void inorder(Node *root, vector<int> &ans)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left, ans);
+    ans.push_back(root->data);
+    inorder(root->right, ans);
+}
+// Function to return a list containing the inorder traversal of the tree.
+vector<int> inOrder(Node *root)
+{
+    // Your code here
+    vector<int> ans;
+    inorder(root, ans);
+    return ans;
+}

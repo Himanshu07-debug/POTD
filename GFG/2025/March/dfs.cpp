925ae2f29c37a5d70ff086157849dfbc5bc88b02
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2025-04-01 22:09:09 +0530

    Create dfs.cpp

---
+++ GFG/2025/March/dfs.cpp
@@ -0,0 +1,27 @@
+int n;
+vector<bool> vis;
+vector<int> ans;
+
+void dfs(int node, vector<vector<int>> &adj)
+{
+    vis[node] = 1;
+    ans.push_back(node);
+    for (auto &ngbr : adj[node])
+    {
+        if (!vis[ngbr])
+            dfs(ngbr, adj);
+    }
+}
+
+vector<int> dfs(vector<vector<int>> &adj)
+{
+    // Code here
+    n = adj.size();
+    vis.resize(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+            dfs(i, adj);
+    }
+    return ans;
+}
